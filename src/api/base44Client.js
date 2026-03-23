25ccd2568fb5a8ccad50634c849540f5184f4ad0
Kennyi00 <youremail@gmail.com>
2026-03-23 14:35:37 -0400

    Fix base44Client.js

--- src/api/base44Client.js
+++ src/api/base44Client.js
@@ -1,21 +1,13 @@
-import CRTOverlay from "../components/retro/CRTOverlay";
-import VHSBadge from "../components/retro/VHSBadge";
-import HeroSection from "../components/retro/HeroSection";
-import AboutSection from "../components/retro/AboutSection";
-import GallerySection from "../components/retro/GallerySection";
-import DevLogSection from "../components/retro/DevLogSection";
-import FooterSection from "../components/retro/FooterSection";
+import { createClient } from '@base44/sdk';
+import { appParams } from '@/lib/app-params';
 
-export default function Home() {
-  return (
-    <div className="min-h-screen bg-background relative">
-      <CRTOverlay />
-      <VHSBadge />
-      <HeroSection />
-      <AboutSection />
-      <GallerySection />
-      <DevLogSection />
-      <FooterSection />
-    </div>
-  );
-}
+const { appId, token, functionsVersion, appBaseUrl } = appParams;
+
+export const base44 = createClient({
+  appId,
+  token,
+  functionsVersion,
+  serverUrl: '',
+  requiresAuth: false,
+  appBaseUrl
+});
